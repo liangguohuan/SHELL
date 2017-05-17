4737c7aa14e70ad6a12643ef4d7d20d94d6485bc
liangguohuan <liangguohuan@gmail.com>
2017-05-17 14:33:07 +0800

    add git submodule libs/bash-argsparse

--- libs/bootstrap.sh
+++ libs/bootstrap.sh
@@ -7,6 +7,7 @@
 
 LIBSDIR=/home/hanson/CodeHub/SHELL/libs
 
+source "${LIBSDIR}/bash-argsparse/argsparse.sh"
 source "${LIBSDIR}/bash-common-helpers.sh"
 source "${LIBSDIR}/bsfl.sh"
 source "${LIBSDIR}/common.sh"
